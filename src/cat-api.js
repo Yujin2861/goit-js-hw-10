40af97f04194ede13656041157b81bc02ebd4e3e
Romaniuk Yevhen <greenapteka.com.ua@gmail.com>
2023-09-24 11:47:30 +0200

    End

---
+++ src/cat-api.js
@@ -0,0 +1,23 @@
+
+const URL = 'https://api.thecatapi.com/v1';
+const API_KEY = "live_i0tDaOGInqQ26rh7JUwZgUxN2ia9tgGfeQAmLlGg5e4sjZuijJKVoZO121TcrnQ9";
+
+export function fetchBreeds() {
+    return fetch(`${URL}/breeds?api_key=${API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });       
+};
+
+export function fetchCatByBreed(breedId) {
+    return fetch(`${URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });  
+};
